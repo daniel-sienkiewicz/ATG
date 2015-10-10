90bfad2b8a774f1624057774beacd8d732601273
Daniel Sienkiewicz <sienkiewicz@project-midas.com>
2015-10-10 18:17:44 +0200

    #1 adding new V

--- zad1.c
+++ zad1.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-void printMatrix(int *tab, int * size){
+void printMatrix(int *tab, int size){
 	int i, j;
 
 	printf("    ");
-	for(i = 0; i < *size; i++)
+	for(i = 0; i < size; i++)
 		printf("%i  ", i);
 
 	printf("\n");
-	for(i = 0; i < *size; i++){
+	for(i = 0; i < size; i++){
 		printf("%i  ", i);
-    	for(j = 0; j < *size; j++)
-    		printf("|%i ", tab[i * (*size) + j]);
+    	for(j = 0; j < size; j++)
+    		printf("|%i ", tab[i * size + j]);
     	printf("|\n");
     }
 }
@@ -36,14 +36,22 @@ void addE(int *tab, int * size){
 	int x, y;
 	printf("Podaj krawedz do dodania: ");
 	scanf("%i %i", &x, &y);
-	tab[x * (*size) + y] = 1;
+	if(!tab[x * (*size) + y]){
+		tab[x * (*size) + y] = 1;
+	} else{
+		printf("Krawedz juz istnieje!");
+	}
 }
 
 void deleteE(int *tab, int * size){
 	int x, y;
 	printf("Podaj krawedz do usuniecia: ");
 	scanf("%i %i", &x, &y);
-	tab[x * (*size) + y] = 0;
+	if(tab[x * (*size) + y]){
+		tab[x * (*size) + y] = 0;
+	} else{
+		printf("Nie ma takiej krawedzi!");
+	}
 }
 
 void degV(int *tab, int * size){
@@ -85,48 +93,69 @@ void degV(int *tab, int * size){
 	printf("Max deg to: %i\n", max);
 	printf("Parzystych deg jest: %i\n", parzy);
 	printf("Nie parzystych deg jest: %i\n", nieparzy);
-	printf("Ciag stopni: ");
+	printf("Ciag deg: ");
 	for(i = 0; i < *size; i++)
     	printf("%i ", degT[i]);
     printf("\n");
 }
 
 void addV(int *tab, int * size){
-	int i, j;
-	int newMatrix[((*size) + 1 ) * ((*size) + 1)];
-	(*size)++;
+	int i, x = 0, count = (*size);
+	int *newMatrix = malloc(((*size) + 1 ) * ((*size) + 1) * sizeof(int));
 
-	for(i = 0; i < *size; i++){
- 	   	for(j = 0; j < *size; j++){
- 	   		newMatrix[i * (*size) + j] = 0;
+	for(i = 0; i < ((count + 1) * (count + 1)); i++){
+    	if((count + (x * (count + 1))) == i){
+    		newMatrix[i] = 0;
+    		x++;
+    	} else{
+    		newMatrix[i] = tab[i - x];
     	}
-    }	
 
-	for(i = 0; i < *size - 1; i++){
- 	   	for(j = 0; j < *size - 1; j++){
- 	   		newMatrix[i * (*size) + j] = tab[i * (*size) + j];
+    	if(i > (count * count) + 1){
+    		newMatrix[i] = 0;
     	}
     }
+  	tab = realloc(tab, (count + 1) * (count + 1) * sizeof(int));
 
-    printMatrix(newMatrix, &(*size));
+  	for(i = 0; i < ((count + 1) * (count + 1)); i++)
+  		tab[i] = newMatrix[i];
 
-    printf("%p %p", &tab, &newMatrix);
+  	free(newMatrix);
+
+    (*size)++;
 }
 
 void deleteV(int *tab, int * size){
+	int i, nr, count = (*size);
+	printf("Ktory wierzcholek usuwamy? ");
+	scanf("%i", &nr);
+	int * newMatrix = malloc(count * count * sizeof(int));
 
+	for(i = 0; i < ((count) * (count)); i++)
+  		newMatrix[i] = tab[i];
+
+	if(nr == count - 1){
+		tab = realloc(tab, (count - 1) * (count - 1) * sizeof(int));
+		
+    } else{
+    	
+	}
+
+	(*size)--;
 }
 
 int main(void){
 	FILE *data;
 	int size, i, j, select = 1;
+	printf("\033[2J");
+    printf("\033[0;0f");
 	printf("Wcztanie z pliku...\n");
     if ((data = fopen("data", "r")) == NULL)
     	printf ("Nie mogę otworzyć pliku data!\n");
 
 	fscanf(data, "%d", &size);
 	printf("Rozmiar: %i\n", size);
-    int matrix[size * size];
+    int *matrix = malloc(size * size * sizeof(int));
 
     // Load data into table from file
     for(i = 0; i < size; i++)
@@ -135,7 +164,7 @@ int main(void){
 
     fclose (data);
     // Print loaded data
-    printMatrix(matrix, &size);
+    printMatrix(matrix, size);
 
 	// Menu	
     while(select){
@@ -150,16 +179,20 @@ int main(void){
     		case 1:
     			printf("1 - dodaj krawedz\n");
     			printf("2 - usun krawedz\n");
+    			printf("3 - powrot\n");
+    			printf("0 - koniec\n");
     			printf("\nCo robimy: ");
     			scanf("%i", &select);
     			switch(select){
     				case 1:
     					addE(matrix, &size);
-    					printMatrix(matrix, &size);
+    					printMatrix(matrix, size);
     					break;
     				case 2:
     					deleteE(matrix, &size);
- 			   			printMatrix(matrix, &size);
+ 			   			printMatrix(matrix, size);
+    					break;
+    				default:
     					break;
     			}
     			break;
@@ -167,29 +200,34 @@ int main(void){
     			printf("1 - dodaj wierzcholek\n");
     			printf("2 - usun wierzcholek\n");
     			printf("3 - stopien wierzcholka\n");
+    			printf("4 - powrot\n");
+    			printf("0 - koniec\n");
     			printf("\nCo robimy: ");
     			scanf("%i", &select);
     			switch(select){
     				case 1:
     					addV(matrix, &size);
-    					printMatrix(matrix, &size);
+    					printMatrix(matrix, size);
     					break;
     				case 2:
     					deleteV(matrix, &size);
- 			   			printMatrix(matrix, &size);
+ 			   			printMatrix(matrix, size);
     					break;
     				case 3:
     					degV(matrix, &size);
     					break;
+    				default:
+    					break;
     			}
     			break;
     		case 3:
-    			printMatrix(matrix, &size);
+    			printMatrix(matrix, size);
     			break;
     		default:
     			select = 0;
     			break;
     	}
     }
+    free(matrix);
 	return 0;
 }
